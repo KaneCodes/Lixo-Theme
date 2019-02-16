1539429a75f2a2474702a0310d4f58cb2963660c
Kane Codes <kane.suckling@gmail.com>
2019-02-16 10:27:27 +0700

    Worked on menu button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
+// DOM Elements
+const menuOverlay = document.querySelector('.menu-overlay');
+const menuBtn = document.querySelector('.menu-btn');
+const menuLine = document.querySelectorAll('.btn-line');
 
+// Default Menu State
+let showMenu = false;
+
+
+
+menuBtn.addEventListener('click', function(){
+  displayMenu();
+  colorChange();
+})
+
+// Display Navigation Menu
+function displayMenu() {
+  if(!showMenu) {
+    menuOverlay.classList.add('show');
+    menuBtn.classList.add('show');
+    showMenu = true;
+  } else {
+    menuOverlay.classList.remove('show');
+    menuBtn.classList.remove('show');
+    showMenu = false;
+  }
+}
+
+
+// Change menu color
+function colorChange() {
+  menuLine.forEach(function(line) {
+    if(showMenu) {
+      line.style.backgroundColor = "#4EA9F5";
+    } else {
+      line.style.backgroundColor = "white";
+    }
+
+  });
+}
